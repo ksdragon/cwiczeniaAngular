de8ce1cfbea91691257b48b62afb17e8d297e51e
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-06-29 13:49:40 +0200

    start app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'addTasks';
+  inputValue;
+  listTasks: Task[] = [];
+  listApprovedTak: Task[] = [];
+
+  approvedTask(task: Task){
+    this.listApprovedTak.push(task);
+  }
+
+  addTask(inputVal: string){
+    let newTask = new Task(inputVal, false);
+    this.listTasks.push(newTask);
+    this.inputValue = '';
+  }
+
+  deleteTask(index: number){
+    this.listTasks.splice(index, 1);
+  }
+
+  //inna metoda usuwania przez metode filter
+  remove(task: Task){
+    this.listTasks = this.listTasks.filter( e => e !== task);
+  }
+
+}
+
+class Task {
+  constructor(public title: string, public statusTask: boolean){};
 }
