24888ac63515303b1c3dbe1fb9d603f46f0b6f6f
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-07-30 13:54:22 +0200

    http post fetch data

--- src/app/http-start/http-start.component.ts
+++ src/app/http-start/http-start.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Post } from '../model/post.model';
 
 @Component({
   selector: 'app-http-start',
@@ -11,13 +13,15 @@ export class HttpStartComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+      .post<{ name: string}>(
+        'https://angular-cwiczenia.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +31,31 @@ export class HttpStartComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get<{[key: string]: Post}>('https://angular-cwiczenia.firebaseio.com/posts.json')
+    .pipe(map(
+      (responseDate: {[key: string]: Post}) => {
+        const postArray: Post[] = [];
+        for (const key in responseDate) {
+          if (responseDate.hasOwnProperty(key)) {
+            postArray.push({ ...responseDate[key], id: key});
+          }
+        }
+        return postArray;
+      }
+    ))
+    .subscribe(
+      posts => {
+        this.loadedPosts = posts;
+        console.log(posts);
+      }
+    );
   }
 }
